958cbd327ca8cb67996c42d36f2dd770afc96536
DevAbas <abas.memmedov.94@gmail.com>
2019-07-20 14:59:17 +0400

    New features added

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,56 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import React, { useContext } from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Context from '../../context';
+import { ME_QUERY } from '../../graphql/queries';
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const { dispatch } = useContext(Context);
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: { authorization: idToken },
+      });
+
+      const { me } = await client.request(ME_QUERY);
+      dispatch({ type: 'LOGIN_USER', payload: me });
+      dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
+    } catch (err) {
+      onFailure(err);
+    }
+  };
+
+  const onFailure = err => {
+    console.error('Error logging in', err);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography component='h1' variant='h3' gutterBottom noWrap>
+        Welcome to GeoPins
+      </Typography>
+      <GoogleLogin
+        clientId='878718101844-9p0qdpo8tb1g88oeermhn8fs9hmih3o3.apps.googleusercontent.com'
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        theme='dark'
+      />
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
 };
 
 export default withStyles(styles)(Login);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const resolvers = require('./resolvers');
 const typeDefs = require('./typeDefs');
+const { findOrCreateUser } = require('./controllers/userController');
 
 // Connection to mongoDB
 mongoose
@@ -14,6 +15,19 @@ mongoose
 const server = new ApolloServer({
   resolvers,
   typeDefs,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
+    }
+    return { currentUser };
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server running on ${url}`));
