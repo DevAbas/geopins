37336faa21fbc987b25d27206163b57b3a0b5a43
DevAbas <abas.memmedov.94@gmail.com>
2019-07-23 12:11:07 +0400

    Uploading image processing has been completed with cloudinary

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,55 +1,151 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhotoTwoTone';
+import LandscapeIcon from '@material-ui/icons/LandscapeOutlined';
+import ClearIcon from '@material-ui/icons/Clear';
+import SaveIcon from '@material-ui/icons/SaveTwoTone';
+
+import Context from '../../context';
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  const { dispatch } = useContext(Context);
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleDeleteDraft = () => {
+    setTitle('');
+    setImage('');
+    setContent('');
+    dispatch({
+      type: 'DELETE_DRAFT',
+    });
+  };
+
+  const handleUploadImage = async () => {
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'geopins');
+    data.append('cloud_name', 'abasthrcodes');
+    const res = await axios.post(
+      'https://api.cloudinary.com/v1_1/abasthrcodes/image/upload',
+      data
+    );
+
+    return res.data.url;
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const url = await handleUploadImage();
+    console.log({ title, image, url, content });
+  };
+
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component='h2'
+        variant='h4'
+        color='secondary'>
+        <LandscapeIcon className={classes.iconLarge} /> Pin Location
+      </Typography>
+      <div>
+        <TextField
+          name='title'
+          label='Title'
+          placeholder='Insert pin title'
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          accept='image/*'
+          id='image'
+          type='file'
+          className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
+        />
+        <label htmlFor='image'>
+          <Button component='span' size='small' className={classes.button}>
+            <AddAPhotoIcon style={{ color: image && 'green' }} />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name='content'
+          label='Content'
+          multiline
+          rows='6'
+          margin='normal'
+          fullWidth
+          variant='outlined'
+          onChange={e => setContent(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          className={classes.button}
+          variant='contained'
+          color='primary'
+          onClick={handleDeleteDraft}>
+          <ClearIcon className={classes.leftIcon} /> Discard
+        </Button>
+        <Button
+          type='submit'
+          className={classes.button}
+          variant='contained'
+          color='secondary'
+          disabled={!title.trim() || !image || !content.trim()}
+          onClick={handleSubmit}>
+          Submit <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
   form: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    paddingBottom: theme.spacing.unit
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingBottom: theme.spacing.unit,
   },
   contentField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: "95%"
+    width: '95%',
   },
   input: {
-    display: "none"
+    display: 'none',
   },
   alignCenter: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   iconLarge: {
     fontSize: 40,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   leftIcon: {
     fontSize: 20,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   rightIcon: {
     fontSize: 20,
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing.unit,
   },
   button: {
     marginTop: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2,
     marginRight: theme.spacing.unit,
-    marginLeft: 0
-  }
+    marginLeft: 0,
+  },
 });
 
 export default withStyles(styles)(CreatePin);
