15709888bf3871a70b663e9df1986cf1e190b959
DevAbas <abas.memmedov.94@gmail.com>
2019-07-25 21:22:15 +0400

    CREATE_COMMENT_MUTATION

--- client/src/components/Comment/CreateComment.js
+++ client/src/components/Comment/CreateComment.js
@@ -1,32 +1,74 @@
-import React from "react";
-import { withStyles } from "@material-ui/core";
-// import InputBase from "@material-ui/core/InputBase";
-// import IconButton from "@material-ui/core/IconButton";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SendIcon from "@material-ui/icons/Send";
-// import Divider from "@material-ui/core/Divider";
+import React, { useState, useContext } from 'react';
+import { withStyles } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import SendIcon from '@material-ui/icons/Send';
+import Divider from '@material-ui/core/Divider';
+
+import { CREATE_COMMENT_MUTATION } from '../../graphql/mutations';
+import { useClient } from '../../client';
+import Context from '../../context';
 
 const CreateComment = ({ classes }) => {
-  return <div>CreateComment</div>;
+  const [comment, setComment] = useState('');
+  const client = useClient();
+  const { state, dispatch } = useContext(Context);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const variables = { pinId: state.currentPin._id, text: comment };
+    const { createComment } = await client.request(
+      CREATE_COMMENT_MUTATION,
+      variables
+    );
+    dispatch({ type: 'CREATE_COMMENT', payload: createComment });
+    setComment('');
+  };
+  return (
+    <>
+      <form className={classes.form}>
+        <IconButton
+          disabled={!comment.trim()}
+          className={classes.clearButton}
+          onClick={() => setComment('')}>
+          <ClearIcon />
+        </IconButton>
+        <InputBase
+          className={classes.input}
+          multiline={true}
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+        <IconButton
+          disabled={!comment.trim()}
+          className={classes.sendButton}
+          onClick={e => handleSubmit(e)}>
+          <SendIcon />
+        </IconButton>
+      </form>
+      <Divider />
+    </>
+  );
 };
 
 const styles = theme => ({
   form: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   input: {
     marginLeft: 8,
-    flex: 1
+    flex: 1,
   },
   clearButton: {
     padding: 0,
-    color: "red"
+    color: 'red',
   },
   sendButton: {
     padding: 0,
-    color: theme.palette.secondary.dark
-  }
+    color: theme.palette.secondary.dark,
+  },
 });
 
 export default withStyles(styles)(CreateComment);
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const { findOrCreateUser } = require('./controllers/userController');
 
 // Connection to mongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('Connected to db'))
   .catch(err => console.log(err));
 
